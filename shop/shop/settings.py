cbdfae23989160d86ae49197dae7b4448816075d
ahmadabd <ahmadabd13741112@gmail.com>
2018-10-14 21:18:52 +0330

    Adding model settings to settings.py(second app)

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shopDB',
+        'PASSWORD':  'a142332144113',
+        'USER': 'postgres',
+        'port': 5432
+     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators

f20a930c085f6322163c0cd3a220f7d5343a0b08
ahmadabd <ahmadabd13741112@gmail.com>
2018-11-07 15:14:14 +0330

    Using template in django for send db infos to html code

--- shop/blog/views.py
+++ shop/blog/views.py
@@ -5,10 +5,15 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
 
-def index(request):
+
+def index(request):    
     latest_post_list = Post.objects.order_by('-published')[:5]
-    output = ', \n'.join([i.slug for i in latest_post_list])
-    return HttpResponse(output)
+    # output = ', \n'.join([i.slug for i in latest_post_list])
+
+    template = loader.get_template("index.html")
+    context = { 'latest_post_list' : latest_post_list }
+    return HttpResponse(template.render(context))
+
 
 def detail(request, post_id):
     # try:
@@ -19,4 +24,6 @@ def detail(request, post_id):
     post = get_object_or_404(Post, pk = post_id)
     template = loader.get_template("detail.html")
 
-    return HttpResponse(template.render())
+    context = { 'post' : post }
+
+    return HttpResponse(template.render(context, request))

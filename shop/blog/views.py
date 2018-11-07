7c626e73e59c9281bf07c28d87cb81a3b999ba30
ahmadabd <ahmadabd13741112@gmail.com>
2018-11-07 15:20:42 +0330

    using DRY for having better code, Now we send data more clear

--- shop/blog/views.py
+++ shop/blog/views.py
@@ -1,29 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import Http404
 from .models import Post
 
+def index(request):
 
-def index(request):    
     latest_post_list = Post.objects.order_by('-published')[:5]
-    # output = ', \n'.join([i.slug for i in latest_post_list])
-
-    template = loader.get_template("index.html")
     context = { 'latest_post_list' : latest_post_list }
-    return HttpResponse(template.render(context))
+    return render(request, 'index.html', context)
 
 
 def detail(request, post_id):
-    # try:
-    #    post = Post.objects.get(pk = post_id)
-    # except Post.DoesNotExist:
-    #    raise Http404("Post does not exists.")
-
     post = get_object_or_404(Post, pk = post_id)
-    template = loader.get_template("detail.html")
-
     context = { 'post' : post }
-
-    return HttpResponse(template.render(context, request))
+    return render(request, 'detail.html', context)

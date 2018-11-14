4f4d42f5987dce473a5f7a19f1ea30436ba29e5f
ahmadabd <ahmadabd13741112@gmail.com>
2018-11-14 17:41:10 +0330

    making converter instead of using regex

--- shop/blog/urls.py
+++ shop/blog/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, register_converter
 from . import views
+
+from extensions import converters
+
+register_converter(converters.fourDigitYear, 'yyyy')
 
 app_name = "blog"
 urlpatterns = [
@@ -11,6 +15,7 @@ urlpatterns = [
     path('<int:post_id>', views.detail, name = 'detail'),
 
     # ex: hostname/blog/archive/2018
-    path('archive/<int:year>/', views.archiveYear, name = 'archive'),
+    #path('archive/<int:year>/', views.archiveYear, name = 'archive'),
+    path('archive/<yyyy:year>/', views.archiveYear, name = 'archive'),
     #re_path(r'^archive/(?p<year>[0-9]{4})/$', views.archiveYear, name = 'archive'),      # using re_path
 ]

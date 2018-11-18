d94542d10e1317d130915e036746ac952c00d976
ahmadabd <ahmadabd13741112@gmail.com>
2018-11-18 11:21:32 +0330

    Using Decorator

--- shop/blog/views.py
+++ shop/blog/views.py
@@ -3,19 +3,22 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import Http404
 from .models import Post
+from django.views.decorators.http import require_http_methods, require_GET, require_safe
 
+
+@require_http_methods(["GET"])
 def index(request):
     latest_post_list = Post.objects.order_by('-published')[:5]
     context = { 'latest_post_list' : latest_post_list }
     return render(request, 'index.html', context)
 
-
+@require_GET
 def detail(request, post_id):
     post = get_object_or_404(Post, pk = post_id)
     context = { 'post' : post }
     return render(request, 'detail.html', context)
 
-
+@require_safe
 def archiveYear(request, year):
 
     year_archive_post = get_list_or_404(Post, published__year = year)    # instead of Post.objects.filter(published__year = year)
